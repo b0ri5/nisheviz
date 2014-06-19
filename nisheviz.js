3e4d3ab7dbe7314f06f6e16fda77cfc3c455e231
Greg Tener <borisb0ri5@gmail.com>
2014-06-19 13:49:52 +0000

    Add a bit for drawing the bbox of some svg.

--- nisheviz.js
+++ nisheviz.js
@@ -1,15 +1,39 @@
 define([], function() {
   'use strict';
-  function SvgPartitionRenderer(svgDocArg) {
-    var svgDoc = svgDocArg;
 
+  var drawBbox = function(svg, draw) {
+    var bbox = svg.bbox();
+    draw.rect(bbox.width, bbox.height)
+        .fill('none')
+        .stroke({width:1})
+        .move(bbox.x, bbox.y);
+  };
+
+  function SvgPartitionRenderer(svgDoc) {
     this.render = function(p) {
       var draw = svgDoc.nested();
       var domain = p.domain();
-      return draw.text(domain.join(' '));
+      for (var i = 0; i < domain.length; i++) {
+        var x = domain[0];
+        var svgX = draw.text(x);
+        drawBbox(svgX, draw);
+        break;
+      }
+      return draw; // draw.text(domain.join(' '));
     };
   }
+
+  function RenderedSvgPartition(partition, elementToSvg, splitToSvg) {
+    this.svgForElement = function(v) {
+      return elementToSvg[v];
+    }
+
+    this.svgForSplit = function(u, v) {
+      return spiltToSvg[[partition.image(u), partition.image(v)]];
+    }
+  }
   return {
+    RenderedSvgPartition : RenderedSvgPartition,
     SvgPartitionRenderer: SvgPartitionRenderer
   };
 });

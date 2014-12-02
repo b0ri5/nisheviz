e8ddce13a40345a8a1b10422cd529f1a3a7d5dce
Greg Tener <borisb0ri5@gmail.com>
2014-12-02 07:03:01 +0000

    Start the new stuff

--- nisheviz.js
+++ nisheviz.js
@@ -1,44 +1,12 @@
 define([], function() {
   'use strict';
 
-  var drawBbox = function(svg, draw) {
-    var bbox = svg.bbox();
-    draw.rect(bbox.width, bbox.height)
-        .fill('none')
-        .stroke({width: 1})
-        .move(bbox.x, bbox.y);
+  var isRefineStart = function(state) {
+    var keys = Object.keys(state);
+    return keys.length == 1 && keys[0] == 'partition';
   };
 
-  function SvgPartitionRenderer(svgDoc) {
-    this.render = function(p) {
-      var draw = svgDoc.nested();
-      var domain = p.domain();
-      var prevBbox;
-      for (var i = 0; i < domain.length; i++) {
-        var x = domain[0];
-        console.log(x);
-        var svgX = draw.text(x);
-        if (prevBbox) {
-          svgX = svgX.move(prevBbox.x + prevBbox.width, prevBbox.y);
-        }
-        prevBbox = svgX.bbox();
-        drawBbox(svgX, draw);
-      }
-      return draw; // draw.text(domain.join(' '));
-    };
-  }
-
-  function RenderedSvgPartition(partition, elementToSvg) {
-    this.svgForElement = function(v) {
-      return elementToSvg[v];
-    };
-
-    /*this.svgForSplit = function(u, v) {
-      return spiltToSvg[[partition.image(u), partition.image(v)]];
-    };*/
-  }
   return {
-    RenderedSvgPartition: RenderedSvgPartition,
-    SvgPartitionRenderer: SvgPartitionRenderer
+    isRefineStart: isRefineStart
   };
 });
--- nisheviz_test.js
+++ nisheviz_test.js
@@ -1,115 +1,33 @@
 define([
   'chai',
-  'jquery',
-  'nishe',
   'nisheviz',
-  'mocha',
-  'svgjs'], function(
+  'mocha'], function(
     chai,
-    $,
-    nishe,
     nisheviz) {
   'use strict';
   var expect = chai.expect;
   describe('nisheviz', function() {
-    describe('next', function() {
-      var state = {
-        elements: ['a', 'b', 'c'],
-        cell_indexes: [],
-        state: 
-      };
-      var nisheviz.next(state);
-    });
-    describe('SvgPartitionRenderer', function() {
-      describe('#render', function() {
-        it('gives wider partitions wider bboxes', function() {
-          $(document).ready(function() {
-            console.log('ready');
-          });
-          var id = 'should-be-wider-than';
-          $('#svg').append('<div id=' + id + '></div>');
-          var ida = id + '-a';
-          $('#' + id).append('<span id=' + ida + '></span>');
-          $('#' + id).append('<p style=display:inline>' + id + '</p>');
-          console.log($('#' + id).length);
-          console.log($('#' + ida).length);
-          var drawA = SVG(ida);
-          var renderer = new nisheviz.SvgPartitionRenderer(drawA);
-          var a = new nishe.Partition([['a']]);
-          var aSvg = renderer.render(a);
-          console.log(drawA);
-          console.log(drawA.viewbox());
-          var viewbox = drawA.viewbox();
-          var bigbbox;
-          drawA.each(function(i, children) {
-            console.log('Child ' + children[i]);
-            if (bigbbox) {
-              bigbbox = bigbbox.merge(children[i].bbox());
-            } else {
-              bigbbox = children[i].bbox();
-            }
-            console.log('big bbox == at ' + i);
-            console.log(bigbbox);
-            console.log('r bbox == at ' + i);
-            console.log(children[i].rbox());
-          });
-          //drawA.viewbox(viewbox.x, viewbox.y, aSvg.bbox().width, aSvg.bbox().height, 1);
-          //drawA.viewbox(viewbox.x, 0, aSvg.bbox().width, aSvg.bbox().height, 1);
-          console.log(drawA.viewbox());
-        
-          $('#' + ida).height(aSvg.bbox().height).width(aSvg.bbox().width);
-          var sleep = function(milliseconds) {
-            var start = new Date().getTime();
-            for (var i = 0; i < 1e7; i++) {
-              if ((new Date().getTime() - start) > milliseconds){
-                break;
-              }
-            }
+    describe('isRefineStart', function() {
+      it('should return true for refine start states', function() {
+        var state = {
+          partition: {
+            elements: [],
+            indexes: []
           }
-          sleep(1000);
-          console.log($('#' + ida).width() + ' - ' + $('#' + ida).height());
-          console.log(aSvg.bbox().height);
-          console.log(aSvg.bbox().width);
-          var x = 5;
-          drawA.size(aSvg.bbox().width + x, aSvg.bbox().height + x);
-//          drawA.size(300, 400);
-/*          renderer = new nisheviz.SvgPartitionRenderer(SVG(idb));
-          var ab = new nishe.Partition([['a', 'b']]);
-          var abSvg = renderer.render(ab);
-          $('#' + idb).height(abSvg.bbox().height);
-          console.log('#' + idb);
-          console.log($('#' + idb));
-          console.log('setting height to ' + $('#' + idb).height());
-          $('#' + idb).width(abSvg.bbox().width);
-          expect(aSvg.bbox().width).to.be.lessThan(abSvg.bbox().width);*/
-        });
-/*        it('gives shorter partitions shorter bboxes', function() {
-          var renderer = new nisheviz.SvgPartitionRenderer(SVG('svg'));
-          var a = new nishe.Partition([['a']]);
-          var aSvg = renderer.render(a);
-          var b = new nishe.Partition([['b']]);
-          var bSvg = renderer.render(b);
-          expect(bSvg.bbox().height).to.not.be.lessThan(aSvg.bbox().height);
-        });
-        it('gives finer partitions wider bboxes', function() {
-          var renderer = new nisheviz.SvgPartitionRenderer(SVG('svg'));
-          var ab = new nishe.Partition([['a', 'b']]);
-          var abSvg = renderer.render(ab);
-          var abFiner = new nishe.Partition([['a'], ['b']]);
-          var abFinerSvg = renderer.render(abFiner);
-          expect(abSvg.bbox().width)
-            .to.not.be.lessThan(abFinerSvg.bbox().width);
-        });
-        it('gives identical partitions identical bboxes', function() {
-          var renderer = new nisheviz.SvgPartitionRenderer(SVG('svg'));
-          var a = new nishe.Partition([['a']]);
-          var aSvg = renderer.render(a);
-          var a2 = new nishe.Partition([['a']]);
-          var a2Svg = renderer.render(a2);
-          expect(aSvg.bbox().width).to.equal(a2Svg.bbox().width);
-          expect(aSvg.bbox().height).to.equal(a2Svg.bbox().height);
-        });*/
+        };
+        expect(nisheviz.isRefineStart(state)).to.equal(true);
       });
+      it('should return false for non refine start states', function() {
+        var state = {
+          partition: {
+            elements: [],
+            indexes: []
+          },
+          activeIndexes: []
+        };
+        expect(nisheviz.isRefineStart(state)).to.equal(false);
+      });
+
     });
   });
 });

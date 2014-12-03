c6df64f9e180fb4fbe3be5839edc09ca696ebf12
Greg Tener <borisb0ri5@gmail.com>
2014-12-03 08:26:50 +0000

    Complete nextChooseActiveIndex

--- nisheviz.js
+++ nisheviz.js
@@ -1,12 +1,76 @@
 define([], function() {
   'use strict';
 
+  var hasExactProperties = function(obj) {
+    var keys = Object.keys(obj);
+    keys.sort();
+    var args = [];
+    for (var i = 1; i < arguments.length; i++) {
+      args.push(arguments[i]);
+    }
+    args.sort();
+    if (keys.length != args.length) {
+      return false;
+    }
+    for (i = 0; i < keys.length; i++) {
+      if (keys[i] != args[i]) {
+        return false;
+      }
+    }
+    return true;
+  };
+
   var isRefineStart = function(state) {
+    return hasExactProperties(state, 'partition') ||
+        hasExactProperties(state, 'partition', 'individualizedIndex');
+  };
+
+  var isChooseActiveIndex = function(state) {
     var keys = Object.keys(state);
-    return keys.length == 1 && keys[0] == 'partition';
+    return keys.indexOf('activeIndexes') != -1 && keys.indexOf('activeIndex') == -1;
+  };
+
+  var next = function(state) {
+    if (isRefineStart(state)) {
+      return nextRefineStart(state);
+    }
+    if (isChooseActiveIndex(state)) {
+      return nextChooseActiveIndex(state);
+    }
+  };
+
+  var nextRefineStart = function(state) {
+    return {
+      partition: state.partition,
+      activeIndexes: state.partition.indexes()
+    };
+  };
+
+  var nextChooseActiveIndex = function(state) {
+    var smallestActiveIndex = state.activeIndexes[0];
+    var smallestActiveIndexSize = state.partition.indexSize(smallestActiveIndex);
+    var smallestActiveIndexIndex = 0;
+    for (var i = 1; i < state.activeIndexes.length; i++) {
+      var activeIndex = state.activeIndexes[i];
+      var size = state.partition.indexSize(activeIndex);
+      if (size < smallestActiveIndexSize) {
+        smallestActiveIndexSize = size;
+        smallestActiveIndex = activeIndex;
+        smallestActiveIndexIndex = i;
+      }
+    }
+    var activeIndexes = state.activeIndexes.splice(0);
+    activeIndexes.splice(smallestActiveIndexIndex, 1);
+    return {
+      partition: state.partition,
+      activeIndexes: activeIndexes,
+      activeIndex: smallestActiveIndex
+    };
   };
 
   return {
-    isRefineStart: isRefineStart
+    isChooseActiveIndex: isChooseActiveIndex,
+    isRefineStart: isRefineStart,
+    next: next
   };
 });
--- nisheviz_test.js
+++ nisheviz_test.js
@@ -1,8 +1,10 @@
 define([
   'chai',
+  'nishe',
   'nisheviz',
   'mocha'], function(
     chai,
+    nishe,
     nisheviz) {
   'use strict';
   var expect = chai.expect;
@@ -10,24 +12,70 @@ define([
     describe('isRefineStart', function() {
       it('should return true for refine start states', function() {
         var state = {
-          partition: {
-            elements: [],
-            indexes: []
-          }
+          partition: new nishe.Partition([['a']])
+        };
+        expect(nisheviz.isRefineStart(state)).to.equal(true);
+      });
+      it('should return true for refine start states with individualized index', function() {
+        var state = {
+          partition: new nishe.Partition([['a']]),
+          individualizedIndex: 0
         };
         expect(nisheviz.isRefineStart(state)).to.equal(true);
       });
       it('should return false for non refine start states', function() {
         var state = {
-          partition: {
-            elements: [],
-            indexes: []
-          },
-          activeIndexes: []
+          partition: new nishe.Partition([['a']]),
+          somethingElse: []
         };
         expect(nisheviz.isRefineStart(state)).to.equal(false);
       });
-
+    });
+    describe('next', function() {
+      describe('from refine start', function() {
+        it('should add all indexes as active for start refine states without an individualized vertex', function() {
+          var state = {
+            partition: new nishe.Partition([['a'], ['b', 'c']])
+          };
+          var next = nisheviz.next(state);
+          expect(next.activeIndexes).to.have.members([0, 1]);
+        });
+      });
+      describe('from choose active index', function() {
+        it('should choose 0 from [a | b c] with 0, 1', function() {
+          var state = {
+            partition: new nishe.Partition([['a'], ['b', 'c']]),
+            activeIndexes: [0, 1]
+          };
+          var next = nisheviz.next(state);
+          expect(next.activeIndex).to.equal(0);
+          expect(next.activeIndexes).to.have.members([1]);
+        });
+        it('should choose 2 from [a b | c] with 0, 2', function() {
+          var state = {
+            partition: new nishe.Partition([['a', 'b'], ['c']]),
+            activeIndexes: [0, 2]
+          };
+          var next = nisheviz.next(state);
+          expect(next.activeIndex).to.equal(2);
+          expect(next.activeIndexes).to.have.members([0]);
+        });
+      });
+    });
+    describe('isChooseActiveIndex', function() {
+      it('returns true if there are active indexes but no active index', function() {
+        var state = {
+          activeIndexes: [1]
+        };
+        expect(nisheviz.isChooseActiveIndex(state)).to.equal(true);
+      });
+      it('returns false if there are active indexes and an active index', function() {
+        var state = {
+          activeIndexes: [1],
+          activeIndex: 0
+        };
+        expect(nisheviz.isChooseActiveIndex(state)).to.equal(false);
+      });
     });
   });
 });

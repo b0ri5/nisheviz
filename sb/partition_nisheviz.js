472557710781e2374236344a494f5ce31c543d6f
Greg Tener <borisb0ri5@gmail.com>
2015-01-19 12:03:01 -0800

    A little more towards a nisheviz sb example

--- sb/partition_nisheviz.js
+++ sb/partition_nisheviz.js
@@ -1,9 +1,14 @@
 require.config({
   paths: {
+    'd3': 'http://d3js.org/d3.v3.min',
+    'nishe': '../node_modules/nishe',
     'nisheviz': '../nisheviz'
   }
 });
-require(['nisheviz'], function() {
+require(['d3', 'nishe', 'nisheviz'], function(d3, nishe, nisheviz) {
   'use strict';
-  console.log('running');
+  var g = d3.select('svg')
+      .append('g');
+  var p = nishe.Partition([['a', 'b'], ['c']]);
+  nisheviz.renderPartition(p, g);
 });
